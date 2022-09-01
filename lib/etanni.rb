58256e56b22227696849179d3c620f5d4505f014
Nick Schwaderer <nick.schwaderer@shopify.com>
2022-09-01 15:28:27 +0100

    init

---
+++ lib/etanni.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "etanni/version"
+
+module Etanni
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/etanni/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Etanni
+  VERSION = "0.1.0"
+end
